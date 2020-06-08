9f18ee4f67eac75e3483c3d0e526578984b9e86d
jie365@126.com <jie365@126.com>
2020-06-08 19:36:52 +0800

    update

---
+++ 6.headers/consumer2.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangrenjie
+ * Date: 2020/6/8
+ * Time: 下午4:09
+ */
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+
+try {
+    $config = [
+        'host' => 'localhost',
+        'port' => 5672,
+        'user' => 'guest',
+        'passowrd' => 'guest',
+        'vhost' => '/',
+    ];
+
+    //创建连接
+    $connection = new AMQPStreamConnection(...array_values($config));
+
+    //创建通道
+    $channel = $connection->channel();
+
+    //为通道创建交换机
+    $exchangeName = 'log_system_headers';
+    $exchangeType = 'headers';
+    $passive = false;
+    $durable = true;
+    $autoDelete = false;
+    $channel->exchange_declare($exchangeName, $exchangeType, $passive, $durable, $autoDelete);
+
+    //通道每次只取一条消息
+    $channel->basic_qos(null, 1, null);
+
+    //创建临时队列
+    $queueName = '';
+    $tmpQueueName = $channel->queue_declare($queueName)[0] ?? '';
+
+
+    //绑定队列到交换机
+    $routingKeyName = '';
+    $noWait = false;
+    $arguments = [
+        'x-match' => 'any',//all是完全匹配
+        'type' => 'log',
+        'level' => 'notice',
+    ];
+    $channel->queue_bind($tmpQueueName, $exchangeName, $routingKeyName, $noWait, $arguments);
+
+    //消费消息
+    $callback = function (\PhpAmqpLib\Message\AMQPMessage $message) {
+        echo "打印错误消息:" . $message->body;
+        $message->delivery_info['channel']->basic_ack($message->delivery_info['delivery_tag']);
+    };
+
+    $consumerTag = '';
+    $noLocal = false;
+    $noAck = false;//关闭自动消息确认，消费完成后手动确认消费消息。
+    $exclusive = false;
+    $noWait = false;
+    $channel->basic_consume($tmpQueueName, $consumerTag, $noLocal, $noAck, $exclusive, $noWait, $callback);
+
+    while ($channel->is_consuming()) {
+        $channel->wait();
+    }
+
+    $channel->close();
+    $connection->close();
+
+} catch (\Throwable $e) {
+    throw $e;
+}
--- 6.headers/producer.php
+++ 6.headers/producer.php
@@ -29,27 +29,50 @@ try {
     $exchangeName = 'log_system_headers';
     $exchangeType = 'headers';
     $passive = false;
-    $durable = false;//持久化
+    $durable = true;//持久化
     $autoDelete = false;
     $exchange = $channel->exchange_declare($exchangeName, $exchangeType, $passive, $durable, $autoDelete);
 
     //定义路由键
-    $routingKeyName = '';
+    $routingKeyName = '';//headers模式下，routing_key为空
 
-    //定义头信息
-    $headers=[
-
-    ];
 
     //构造消息
-    $content = 'This is a log message [' . $routingKeyName . ']';
+    $content1 = 'This is a log message1 from the exchange which named \'log_system_headers\'';
     $properties = [
-        'dilivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT,
+        'dilivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT,//投递模式
+        'headers' => [//设置消息的header
+            'type' => 'log',
+            'level' => 'error',
+        ],
     ];
-    $message = new AMQPMessage($content, $properties);
+    $message1 = new AMQPMessage($content1, $properties);
+
+    //构造消息
+    $content2 = 'This is a log message2 from the exchange which named \'log_system_headers\'';
+    $properties = [
+        'dilivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT,//投递模式
+        'headers' => [//设置消息的header
+            'type' => 'log',
+            'level' => 'notice',
+        ],
+    ];
+    $message2 = new AMQPMessage($content2, $properties);
+
+    //构造消息
+    $content3 = 'This is a log message3 from the exchange which named \'log_system_headers\'';
+    $properties = [
+        'dilivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT,//投递模式
+        'headers' => [//设置消息的header
+            'type' => 'log',
+            'level' => 'warning',
+        ],
+    ];
+    $message3 = new AMQPMessage($content3, $properties);
 
     //生产者将消息发到到交换机
-    $channel->basic_publish($message, $exchangeName, $routingKeyName);
+    $channel->basic_publish($message1, $exchangeName, $routingKeyName);
+    $channel->basic_publish($message2, $exchangeName, $routingKeyName);
 
     //关闭连接
     $channel->close();
