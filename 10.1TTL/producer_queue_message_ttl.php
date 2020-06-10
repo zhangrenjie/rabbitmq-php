bfbddd462a5907aa5d63d08f5af2f2fe522277dc
jie365@126.com <jie365@126.com>
2020-06-10 11:52:52 +0800

    update

---
+++ 10.1TTL/producer_queue_message_ttl.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangrenjie
+ * Date: 2020/6/5
+ * Time: 下午2:22
+ */
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+try {
+    $config = [
+        'host' => 'localhost',
+        'port' => 5672,
+        'user' => 'guest',
+        'password' => 'guest',
+        'vhost' => '/',
+    ];
+
+    //连接RabbitMQ服务
+    $connection = new AMQPStreamConnection(...array_values($config));
+
+    //创建通道
+    $channel = $connection->channel();
+    $channel->basic_qos(null, 1, null);//通道每次只处理一条消息
+
+    //设置队列的TTL
+    $arguments = new \PhpAmqpLib\Wire\AMQPTable();
+    $arguments->set('x-message-ttl', 60000);//设置队列中消息的过期时间，单位是毫秒
+
+    //通道绑定队列
+    $queueName = 'message_queue_message_ttl';
+    $passive = false;
+    $durable = true;//是否持久化队列，服务重启队列不会丢失
+    $exclusive = false;//是否独占队列，独占队列只允许当前当前连接使用
+    $autoDelete = false;//队列消费完是否自动删除（true当消费者断开服务连接后才会自动删除）
+    $noWait = false;
+    $channel->queue_declare($queueName, $passive, $durable, $exclusive, $autoDelete, $noWait, $arguments);
+
+
+    for ($i = 1; $i <= 1000; $i++) {
+        //构造消息
+        $content = $i . ' -> Hello world , This is rabbitmq work queues model ' . date('Y-m-d H:i:s');
+
+        $properties = [
+            //消息传递模式
+            'delivery_model' => AMQPMessage::DELIVERY_MODE_NON_PERSISTENT,//持久化
+        ];
+        $message = new AMQPMessage($content, $properties);
+
+        //发送消息
+        $exchangeName = '';//交换机名称，空则为系统的默认交换机
+        $routingKeyName = $queueName;//发送消息必须通过交换机指定routing_key发送。
+        $channel->basic_publish($message, $exchangeName, $routingKeyName);
+    }
+
+    echo "消息发送成功" . PHP_EOL;
+
+    //关闭通道
+    $channel->close();
+
+    //关闭连接
+    $connection->close();
+} catch (\Throwable $e) {
+    throw $e;
+}
+
